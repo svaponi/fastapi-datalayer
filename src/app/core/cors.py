fd2ac40c2373cac06e30a4c1096c3bff8abf297e
svaponi <10941963+svaponi@users.noreply.github.com>
2025-12-22 13:20:29 +0100

    task: adapt to CORS_ENABLED env var

--- src/app/core/cors.py
+++ src/app/core/cors.py
@@ -4,19 +4,16 @@ from starlette.middleware.cors import CORSMiddleware
 
 
 class CorsConfig(pydantic_settings.BaseSettings):
-    ENABLED: bool = False
-    ALLOW_ORIGINS: str | None = None
-    ALLOW_ORIGIN_REGEX: str | None = None
-    model_config = pydantic_settings.SettingsConfigDict(
-        env_prefix="CORS_",
-    )
+    CORS_ENABLED: bool = False
+    CORS_ALLOW_ORIGINS: str | None = None
+    CORS_ALLOW_ORIGIN_REGEX: str | None = None
 
 
 def setup_cors(app: fastapi.FastAPI, cors: CorsConfig):
-    if not cors.ENABLED:
+    if not cors.CORS_ENABLED:
         return
-    allow_origins = cors.ALLOW_ORIGINS or ["*"]
-    allow_origin_regex = cors.ALLOW_ORIGIN_REGEX
+    allow_origins = cors.CORS_ALLOW_ORIGINS or ["*"]
+    allow_origin_regex = cors.CORS_ALLOW_ORIGIN_REGEX
     if isinstance(allow_origins, str):
         allow_origins = allow_origins.split(",")
     if allow_origin_regex:
